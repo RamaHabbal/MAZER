e66832b80c3a0222bf537addaefffedc8f489768
Mostafa Kreidly <mostafa@Mostafas-MacBook-Air.local>
2023-07-16 19:44:00 +0300

    changing structure

--- src/scripts/Scene1.js
+++ src/scripts/Scene1.js
@@ -1,40 +1,36 @@
 class Scene1 extends Phaser.Scene {
+  constructor() {
+    super('bootGame');
+  }
 
-    constructor(){
-        super("bootGame");
-    }
+  preload() {
+    this.load.video(
+      'video',
+      './src/assets/images/halloweenbackground.mp4',
+      'loadeddata',
+      false,
+      true
+    );
+    // this.load.image('house', './src/assets/images/house.png');
 
-    preload(){
-        
-        this.load.video('video', './src/assets/images/halloweenbackground.mp4', 'loadeddata', false, true);
-        // this.load.image('house', './src/assets/images/house.png');
-        
-        this.load.image('image', './src/assets/images/logomaze.png');
-        this.load.spritesheet('character', './src/assets/images/spritesheet.png', {
-              frameWidth: 48,
-              frameHeight: 48,
-        });
+    this.load.image('image', './src/assets/images/logomaze.png');
+    this.load.spritesheet('character', './src/assets/images/spritesheet.png', {
+      frameWidth: 48,
+      frameHeight: 48,
+    });
 
-        this.load.spritesheet('portal', './src/assets/images/newportal.png', {
-            frameWidth: 50,
-            frameHeight: 50,
-        });
+    this.load.spritesheet('portal', './src/assets/images/newportal.png', {
+      frameWidth: 50,
+      frameHeight: 50,
+    });
 
-        this.load.image('wall', './src/assets/images/wall.jpeg');
-        this.load.image('ground', './src/assets/images/ground.jpeg');
-        this.load.audio('audio', './src/assets/music/Peder_B_Helland_Darkness.m4a');
-          
-    }
+    this.load.image('wall', './src/assets/images/wall.jpeg');
+    this.load.image('ground', './src/assets/images/ground.jpeg');
+    this.load.audio('audio', './src/assets/music/Peder_B_Helland_Darkness.m4a');
+  }
 
-    create(){
-        this.add.text(20,20,"Loading game...");
-        this.scene.start("Menu");
-       
-    }
-
-  
-
-
-
+  create() {
+    this.add.text(20, 20, 'Loading game...');
+    this.scene.start('Gaming');
+  }
 }
-
--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,32 +1,31 @@
-window.onload=function(){
+window.onload = function () {
+  let config = {
+    type: Phaser.AUTO,
+    width: 800, // Set the desired width of the game canvas
+    height: 600, // Set the desired height of the game canvas
+    parent: 'game-container', // The ID of the HTML element to attach the game canvas to
+    //begin: scaling for game
 
-
-let config = {
-  type: Phaser.AUTO,
-  width: 1000, // Set the desired width of the game canvas
-  height: 1000, // Set the desired height of the game canvas
-  parent: 'game-container', // The ID of the HTML element to attach the game canvas to
-  //begin: scaling for game
-
-  scale:{
-    parent:'game-container',
-    mode: Phaser.Scale.FIT,
-    autoCenter: Phaser.Scale.CENTER_BOTH,
-    zoom:1,
-  },
-
-  //end: scaling for game
-
-  scene: [Scene1, Scene2,Scene3,SettingsMenu,GameOver,Story, Scene4],
-  physics: {
-    default: 'arcade', // Set the default physics system to Arcade Physics
-    arcade: {
-      // Configure the Arcade Physics settings
-      gravity: { y: 0 }, // Disable gravity for a top-down maze game
-      debug: false, // Set to true for debugging collision boundaries
+    scale: {
+      parent: 'game-container',
+      mode: Phaser.Scale.FIT,
+      autoCenter: Phaser.Scale.CENTER_BOTH,
+      zoom: 1,
     },
-  },
-};
 
-const game = new Phaser.Game(config);
-}
+    //end: scaling for game
+
+    // scene: [Scene1, Scene2,Scene3,SettingsMenu,GameOver,Story, Scene4],
+    scene: [Scene1, Scene3],
+    physics: {
+      default: 'arcade', // Set the default physics system to Arcade Physics
+      arcade: {
+        // Configure the Arcade Physics settings
+        gravity: { y: 0 }, // Disable gravity for a top-down maze game
+        debug: false, // Set to true for debugging collision boundaries
+      },
+    },
+  };
+
+  const game = new Phaser.Game(config);
+};
