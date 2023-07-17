cdfd6c4ace344b64587029ee80f3ef2a6efb55ec
MHoussam <mohammadosta1999@gmail.com>
2023-07-17 08:29:23 +0300

    Added the reversed arrows keys in some levels.

--- src/scripts/Sprites/Player.js
+++ src/scripts/Sprites/Player.js
@@ -100,75 +100,93 @@ class Player extends Phaser.Physics.Arcade.Sprite {
     let movementS = this.scene.input.keyboard.addKey("S").isDown;
     let movementD = this.scene.input.keyboard.addKey("D").isDown;
 
-    if (movement_up || movementW) {
-      this.anims.play("up", true); // Play 'up' animation
-      this.setVelocity(0, -this.speed);
-      direction = "up";
+    let up='up', down='down', left='left', right='right';
+    let ghostUp='ghostUp', ghostDown='ghostDown', ghostLeft='ghostLeft', ghostRight='ghostRight';
+    let directionUp='up', directionDown='down', directionLeft='left', directionRight='right';
+    let upY=-this.speed, downY=this.speed, rightX=this.speed, leftX=-this.speed;
+    let GupY=80, GdownY=-80, GleftX=80, GrightX=-80;
 
-      ghost.anims.play("ghostUp", true);
+    if (score % 3 == 0 ) {
+      up = 'down', down = 'up', left = 'right', right = 'left';
+
+      ghostUp = 'ghostDown', ghostDown = 'ghostUp', ghostLeft = 'ghostRight', ghostRight = 'ghostLeft';
+
+      upY = this.speed, downY = -this.speed, rightX = -this.speed, leftX = this.speed;
+
+      directionUp = 'down', directionDown = 'up', directionLeft = 'right', directionRight = 'left';
+
+      GupY = -80, GdownY = 80, GleftX = -80, GrightX = 80;
+    }
+    
+    if (movement_up || movementW) {
+      this.anims.play(up, true); // Play 'up' animation
+      this.setVelocity(0, upY);
+      direction = directionUp;
+
+      ghost.anims.play(ghostUp, true);
       ghost.x = this.body.position.x;
-      ghost.y = this.body.position.y + 80;
+      ghost.y = this.body.position.y + GupY;
 
       if (movement_left || movementA) {
-        this.anims.play("up", true); // Play 'left' animation
-        this.setVelocity(-this.speed, 0);
-        direction = "left";
+        this.anims.play(up, true); // Play 'left' animation
+        this.setVelocity(leftX, 0);
+        direction = directionLeft;
 
-        ghost.anims.play("ghostLeft", true);
-        ghost.x = this.body.position.x + 80;
+        ghost.anims.play(ghostLeft, true);
+        ghost.x = this.body.position.x + GleftX;
         ghost.y = this.body.position.y;
       }
       if (movement_right || movementD) {
-        this.anims.play("up", true); // Play 'right' animation
-        this.setVelocity(this.speed, 0);
-        direction = "right";
+        this.anims.play(up, true); // Play 'right' animation
+        this.setVelocity(rightX, 0);
+        direction = directionRight;
 
-        ghost.anims.play("ghostRight", true);
-        ghost.x = this.body.position.x - 80;
+        ghost.anims.play(ghostRight, true);
+        ghost.x = this.body.position.x + GrightX;
         ghost.y = this.body.position.y;
       }
     } else if (movement_down || movementS) {
-      this.anims.play("down", true); // Play 'down' animation
-      this.setVelocity(0, this.speed);
-      direction = "down";
+      this.anims.play(down, true); // Play 'down' animation
+      this.setVelocity(0, downY);
+      direction = directionDown;
 
-      ghost.anims.play("ghostDown", true);
+      ghost.anims.play(ghostDown, true);
       ghost.x = this.body.position.x;
-      ghost.y = this.body.position.y - 80;
+      ghost.y = this.body.position.y + GdownY;
       if (movement_left || movementA) {
-        this.anims.play("down", true); // Play 'left' animation
-        this.setVelocity(-this.speed, 0);
-        direction = "left";
+        this.anims.play(down, true); // Play 'left' animation
+        this.setVelocity(leftX, 0);
+        direction = directionLeft;
 
-        ghost.anims.play("ghostLeft", true);
-        ghost.x = this.body.position.x + 80;
+        ghost.anims.play(ghostLeft, true);
+        ghost.x = this.body.position.x + GleftX;
         ghost.y = this.body.position.y;
       }
       if (movement_right || movementD) {
-        this.anims.play("down", true); // Play 'right' animation
-        this.setVelocity(this.speed, 0);
-        direction = "right";
+        this.anims.play(down, true); // Play 'right' animation
+        this.setVelocity(rightX, 0);
+        direction = directionRight;
 
-        ghost.anims.play("ghostRight", true);
-        ghost.x = this.body.position.x - 80;
+        ghost.anims.play(ghostRight, true);
+        ghost.x = this.body.position.x + GrightX;
         ghost.y = this.body.position.y;
       }
     } else if (movement_left || movementA) {
-      this.anims.play("left", true); // Play 'left' animation
-      this.setVelocity(-this.speed, 0);
-      direction = "left";
+      this.anims.play(left, true); // Play 'left' animation
+      this.setVelocity(leftX, 0);
+      direction = directionLeft;
 
-      ghost.anims.play("ghostLeft", true);
-      ghost.x = this.body.position.x + 80;
+      ghost.anims.play(ghostLeft, true);
+      ghost.x = this.body.position.x + GleftX;
       ghost.y = this.body.position.y;
     } else if (movement_right || movementD) {
-      this.anims.play("right", true); // Play 'right' animation
-      this.setVelocity(this.speed, 0);
-      direction = "right";
+      this.anims.play(right, true); // Play 'right' animation
+      this.setVelocity(rightX, 0);
+      direction = directionRight;
 
-      ghost.anims.play("ghostRight", true);
+      ghost.anims.play(ghostRight, true);
       console.log(ghost);
-      ghost.x = this.body.position.x - 80;
+      ghost.x = this.body.position.x + GrightX;
       ghost.y = this.body.position.y;
     } else {
       this.setVelocity(0, 0);
